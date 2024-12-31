846b1ded347e23199238f2a3cabdb45b6d84988a
sweta <swetapatil1991@gmail.com>
2024-12-31 15:12:45 +0530

    hello world and time ticking

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,9 +8,21 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    document.getElementById('root');
   </React.StrictMode>
+  
 );
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
 
+setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
